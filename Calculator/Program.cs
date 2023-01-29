1db3450772f3291c988c1658832444d37a1941fd
GRIIZL <72096275+GRIIZL@users.noreply.github.com>
2023-01-29 19:24:04 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double rez;
+            bool check=false;
+
+            Console.WriteLine("Введите превое число");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double z = double.Parse(Console.ReadLine());
+
+            while (check == false) 
+            { 
+             Console.WriteLine("Введите желаемую операцию : *, /, +, -");
+             string oper = Console.ReadLine();
+            
+           
+            if ("*" == oper)
+            {
+               rez = Ymnojenie(x, z);
+                check = true;
+                Console.WriteLine(rez);
+            }
+            if ("/" == oper)
+            {
+                rez = Delenie(x, z);
+                check = true;
+                Console.WriteLine(rez);
+            }
+            if ("+" == oper)
+            {
+                rez = slojenie(x, z);
+                check = true;
+                Console.WriteLine(rez);
+            }
+            if ("-" == oper)
+            {
+                rez = vichitanie(x, z);
+                check = true;
+                Console.WriteLine(rez);
+            }
+            
+            else
+                { Console.WriteLine("Повторите попытку"); }
+            }
+
+
+
+            static double slojenie(double x, double z)
+            {
+                return x + z;
+            }
+            static double vichitanie(double x, double z)
+            {
+                return x - z;
+            }
+            static double Delenie(double x, double z)
+            {
+                return x / z;
+            }
+            static double Ymnojenie(double x , double z)
+            {
+                return x * z;
+            }
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+               
+               
+            
+
+            
+        }
+    }
+}
